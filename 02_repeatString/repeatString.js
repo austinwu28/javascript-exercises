93db68559f5ab018549c7b760f8bcbe473f17f28
Austin Wu <austinwu28@gmail.com>
2022-03-21 11:58:58 -0400

    Complete 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,14 @@
-const repeatString = function() {
+const repeatString = function (phrase, number) {
+
+    if (number < 0) return "ERROR";
+
+    let repeatedString = "";
+
+    for (let i = 0; i < number; i++) {
+        repeatedString += phrase;
+    }
+
+    return repeatedString;
 
 };
 
