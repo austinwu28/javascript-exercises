c5fe9868b596f8a1ef919e5dbc8a77b78197e28f
Austin Wu <austinwu28@gmail.com>
2022-03-21 12:09:48 -0400

    Complete 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,12 @@
-const reverseString = function() {
+const reverseString = function (phrase) {
+
+    let reversedString = "";
+
+    for (let i = phrase.length - 1; i >= 0; i--) {
+        reversedString += phrase.charAt(i);
+    }
+
+    return reversedString;
 
 };
 
